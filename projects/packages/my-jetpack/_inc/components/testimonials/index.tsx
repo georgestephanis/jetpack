4e84f5889feecf41d8e99b7f6714dc6de676e2a0
Dylan Munson <65001528+CodeyGuyDylan@users.noreply.github.com>
2025-04-02 08:50:47 -0700

    Fix/copy on jetpack onboarding testimonials (#42827)
    * Fix issues with copy on onboarding testimonials
    
    * changelog

--- projects/packages/my-jetpack/_inc/components/testimonials/index.tsx
+++ projects/packages/my-jetpack/_inc/components/testimonials/index.tsx
@@ -1,5 +1,6 @@
 import { DotPager } from '@automattic/jetpack-components';
 import { __ } from '@wordpress/i18n';
+import preventWidows from '../../utils/prevent-widows';
 
 import './style.scss';
 
@@ -13,8 +14,8 @@ const Testimonials: React.FC = () => {
 							<p className="jetpack-onboarding-testimonial__message">
 								{ __( 'Grow your audience.', 'jetpack-my-jetpack' ) }
 							</p>
-							<p className="jetpack-onboarding-testimonial__message">
-								{ __( 'Speed up your site', 'jetpack-my-jetpack' ) }
+							<p className="testimonial__message">
+								{ __( 'Speed up your site.', 'jetpack-my-jetpack' ) }
 							</p>
 							<p className="jetpack-onboarding-testimonial__message">
 								{ __( 'Keep it secure.', 'jetpack-my-jetpack' ) }
@@ -22,28 +23,32 @@ const Testimonials: React.FC = () => {
 						</div>
 					</div>
 				</div>
-				<div className="jetpack-onboarding-testimonial jetpack-onboarding-testimonial--2">
-					<div className="jetpack-onboarding-testimonial__content">
-						<p className="jetpack-onboarding-testimonial__quote">
-							{ __(
-								"Jetpack's performance features are no-brainers for the sites I build. With one-click CDN, there's no need to sacrifice performance for style. I know that it just automagically works once I toggle that button.",
-								'jetpack-my-jetpack'
+				<div className="testimonial testimonial--2">
+					<div className="testimonial__content">
+						<p className="testimonial__quote">
+							{ preventWidows(
+								__(
+									"Jetpack's performance features are no-brainers for the sites I build. With one-click CDN, there's no need to sacrifice performance for style. I know that it just automagically works once I toggle that button.",
+									'jetpack-my-jetpack'
+								)
 							) }
 						</p>
 						<p className="jetpack-onboarding-testimonial__author">
 							<strong>{ __( 'Sasha Endoh', 'jetpack-my-jetpack' ) }</strong>
 						</p>
-						<p className="jetpack-onboarding-testimonial__title">
-							{ __( 'Multidisciplinary Designed', 'jetpack-my-jetpack' ) }
+						<p className="testimonial__title">
+							{ __( 'Multidisciplinary Designer', 'jetpack-my-jetpack' ) }
 						</p>
 					</div>
 				</div>
-				<div className="jetpack-onboarding-testimonial jetpack-onboarding-testimonial--3">
-					<div className="jetpack-onboarding-testimonial__content">
-						<p className="jetpack-onboarding-testimonial__quote">
-							{ __(
-								"Millions of people depend on my site, and downtime isn't an option. Jetpack handles my site security and backups so I can focus on creation.",
-								'jetpack-my-jetpack'
+				<div className="testimonial testimonial--3">
+					<div className="testimonial__content">
+						<p className="testimonial__quote">
+							{ preventWidows(
+								__(
+									"Millions of people depend on my site, and downtime isn't an option. Jetpack handles my site security and backups so I can focus on creation.",
+									'jetpack-my-jetpack'
+								)
 							) }
 						</p>
 						<p className="jetpack-onboarding-testimonial__author">
